b75b9c9877e5ebbfdcf7aa76f8e058b7f5470473
geneo924 <genemoore924@gmail.com>
2023-02-24 14:42:52 -0500

    add setup() and logic()

--- snake.c
+++ snake.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 
-int row, column, height = 30;
-int width = 30, gameover, score;
+int height = 30, width = 30;
+int gameover, score, flag;
 int snakex, snakey, foodx, foody;
+
 
 
 void draw(){
@@ -31,14 +33,107 @@ void draw(){
         }
         printf("\n");
     }
+    printf("score = %d", score);
+    printf("\n");
+    printf("press X to quit the game");
 }
 
 void setup(){
     
+    // to randomly place the food anywhere on the board, within boundaries
+    // to handle game state, 
+    gameover = 0;
+    score = 0;
+
+    // start in the middle of the game board
+    snakex = height / 2;
+    snakey = width / 2;
+
+    // always make sure the food is in proper boundaries
+    label1:
+        foodx = rand() % 30;
+        if(foodx == 0)
+            goto label1;
+    label2:
+        foody = rand() % 30;
+        if(foody == 0)
+            goto label2;
+}
+
+void input(){
+    int ch = getchar();
+        switch(ch) {
+            case 'a':
+                flag = 1;
+                break;
+            case 's':
+                flag = 2;
+                break;
+            case 'd':
+                flag = 3;
+                break;
+            case 'w':
+                flag = 4;
+                break;
+            case 'x':
+                gameover = 1;
+                break;
+        }
+    }
+
+void logic(){
+
+    usleep(10000);
+
+    switch(flag) {
+        case 1:
+            snakey--;
+            break;
+        case 2:
+            snakex++;
+            break;
+        case 3:
+            snakey++;
+            break;
+        case 4:
+            snakex--;
+            break;
+        default:
+            break;
+    }
+
+    if(snakex < 0 || snakex > height || snakey < 0 && snakey > width){
+        gameover = 1;
+    }
+
+    // when snake eats the food, generate new food item on board
+    if(snakex == foodx && snakey == foody){
+        label3:
+            foodx = rand() % 30;
+            if(foodx == 0){
+                goto label3;
+            }
+
+
+        label4:
+            foody = rand() % 30;
+            if(foody == 0){
+                goto label4;
+            }
+            score+=10;
+    }
 }
 
 int main(){
 
-    draw();
-    return 0;
+   setup();
+
+   while(!gameover){
+        draw();
+        input();
+        logic();
+   }
+
+   
+   return 0;
 }
